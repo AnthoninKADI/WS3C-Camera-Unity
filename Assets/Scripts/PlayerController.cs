560382b30c738bbfae505d064e51cbc1b89ca6c1
Anthonin <akadi@artfx.fr>
2024-02-21 16:54:24 +0100

    [Add] Inputs & Canvas
    - Run
    - Switch Cam
    - Canvas Inputs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,16 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 8;
+    private float runningSpeed;
+    private float currentSpeed;
     public float rotationSpeed = 40;
     private Vector2 direction;
+
+    void Start() 
+    {
+        currentSpeed = speed;
+        runningSpeed = speed * 2;
+    }
 
     void Update()
     {
@@ -18,5 +26,45 @@ public class PlayerController : MonoBehaviour
     public void move(InputAction.CallbackContext context)
     {
         direction = context.ReadValue<Vector2>();
+    }
+
+    public void SwitchTPS(InputAction.CallbackContext context)
+    {
+        if (context.performed) 
+        {
+            // activer la condition du code pour la camera
+        }
+    }
+
+    public void SwitchFPS(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            // activer la condition du code pour la camera
+        }
+    }
+
+    public void Switch2D(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            // activer la condition du code pour la camera
+        }
+    }
+
+    public void Run(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            speed = runningSpeed;
+            Debug.Log(speed);
+            // ajouter le zoom de camera pendant la course
+        }
+        else if (context.canceled) 
+        {
+            speed = currentSpeed;
+            Debug.Log(speed);
+            // enlever le zoom de camera pendant la course
+        }
     }
 }
